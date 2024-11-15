6eea78b0e27c38cbd0209a65b42c9ee0354b5488
Arthur <arthur.9996@gmail.com>
2024-11-15 16:55:57 +0100

    fixed : batch convert

--- admin/game/process.php
+++ admin/game/process.php
@@ -40,9 +40,9 @@ if(isset($_FILES["game_image"])){
 
         $file = $game_image . "." . $extension;
 
-        var_dump($_FILES["game_image"]);
-
         move_uploaded_file($_FILES["game_image"]["tmp_name"], $path . $file);
+
+        $src_file = $file;
         
         foreach ($img_format as $prefix => $info){
 
@@ -50,8 +50,7 @@ if(isset($_FILES["game_image"])){
             $dest_height = $info["height"];
             $crop = $info["crop"];
 
-            if ($sizes = getimagesize($path . $file)){
-                var_dump($sizes);
+            if ($sizes = getimagesize($path . $src_file)){  
                 
                 $src_width = $sizes[0];
                 $src_height = $sizes[1];
@@ -83,16 +82,16 @@ if(isset($_FILES["game_image"])){
             switch ($extension){
                 case "jpeg":
                 case "jpg":
-                    $src = imagecreatefromjpeg($path . $file);
+                    $src = imagecreatefromjpeg($path . $src_file);
                     break;
                 case "png":
-                    $src = imagecreatefrompng($path . $file);
+                    $src = imagecreatefrompng($path . $src_file);
                     break;
                 case "gif":
-                    $src = imagecreatefromgif($path . $file);
+                    $src = imagecreatefromgif($path . $src_file);
                     break;
                 case "webp":
-                    $src = imagecreatefromwebp($path . $file);
+                    $src = imagecreatefromwebp($path . $src_file);
                     break;
                 default:
                     exit();   
@@ -112,6 +111,11 @@ if(isset($_FILES["game_image"])){
             }
 
             imagewebp($dest, $path . $prefix . $game_image . ".webp", 100);
+
+            if (!$crop){
+                $extension = "webp";
+                $src_file = $prefix . $game_image . "." . $extension;
+            }
         }
 
         deleteFile($path . $file);
@@ -126,24 +130,24 @@ if(isset($_FILES["game_image"])){
     }
 }
 
-exit();
-
 if (isset($_POST["post_sent"]) && ($_POST["post_sent"] == "toto")){
     if ($_POST["game_id"] == 0){
-        $stmt = $db->prepare("INSERT INTO table_game (game_name, game_description, game_editor, game_price, game_type) VALUES (:game_name, :game_description, :game_editor, :game_price, :game_type)");
+        $stmt = $db->prepare("INSERT INTO table_game (game_name, game_description, game_editor, game_price, game_type, game_image) VALUES (:game_name, :game_description, :game_editor, :game_price, :game_type, :game_image)");
         $stmt->execute([":game_name" => $_POST["game_name"],
                                 ":game_description" => $_POST["game_description"],
                                 ":game_editor" => $_POST["game_editor"],
                                 ":game_price" => $_POST["game_price"],
-                                ":game_type" => $_POST["game_type"]]);
+                                ":game_type" => $_POST["game_type"],
+                                ":game_image" => $game_image . ".webp" ]);
     } else {
-        $stmt = $db->prepare("UPDATE table_game SET game_name = :game_name, game_description = :game_description, game_editor = :game_editor, game_price = :game_price, game_type = :game_type WHERE game_id = :game_id");
+        $stmt = $db->prepare("UPDATE table_game SET game_name = :game_name, game_description = :game_description, game_editor = :game_editor, game_price = :game_price, game_type = :game_type, game_image = :game_image WHERE game_id = :game_id");
         $stmt->bindValue(":game_name", $_POST["game_name"]);
         $stmt->bindValue(":game_description", $_POST["game_description"]);
         $stmt->bindValue(":game_editor", $_POST["game_editor"]);
         $stmt->bindValue(":game_price", $_POST["game_price"]);
         $stmt->bindValue(":game_id", $_POST["game_id"]);
         $stmt->bindValue(":game_type", $_POST["game_type"]);
+        $stmt->bindValue(":game_image", $game_image . ".webp");
 
         $stmt->execute();
     }
